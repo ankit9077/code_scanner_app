4b1832971fc320e503ba550cea67ca4fab47ff77
ankit9077 <ankit.bajaj000@gmail.com>
2022-07-12 14:20:34 -0400

    login page added

--- safeRunner/src/app/app-routing.module.ts
+++ safeRunner/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guard/auth.guard';
 
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomeModule)
+    loadChildren: () => import('./home/home.module').then( m => m.HomeModule),
+    canActivate: [AuthGuard]
   },
   {
     path: '',
--- safeRunner/src/app/login/login.page.ts
+++ safeRunner/src/app/login/login.page.ts
@@ -1,4 +1,7 @@
+import { Router } from '@angular/router';
+import { AuthenticationService } from './../services/authentication/authentication.service';
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/assets/models';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  isLoading: boolean;
+  errorMessage = '';
   credentials: {email: string; password: string } = {email: '', password: ''};
-  constructor() { }
+  constructor(private authService: AuthenticationService, private router: Router) {}
 
   ngOnInit() {
+  }
+
+  login() {
+    const user = new User();
+    user.email = this.credentials.email.trim();
+    user.password = this.credentials.password.trim();
+    this.errorMessage = 'starting';
+    this.authService.authenticateUser(user).then((response) => {
+      this.errorMessage = 'success';
+      this.router.navigate(['home']);
+    }, (err) => {
+      this.errorMessage = 'Failed';
+      this.errorMessage = err.error.message;
+    });
   }
 
 }
