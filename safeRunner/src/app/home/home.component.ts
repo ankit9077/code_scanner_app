5ee132748a4bb88b956abda614168cfa139355a6
ankit9077 <ankit.bajaj000@gmail.com>
2022-07-12 16:18:03 -0400

    user service added and binded

--- safeRunner/src/app/home/home.component.ts
+++ safeRunner/src/app/home/home.component.ts
@@ -1,4 +1,7 @@
+import { UserService } from './../services/user/user.service';
+import { AuthenticationService } from './../services/authentication/authentication.service';
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/assets/models';
 
 @Component({
   selector: 'app-home',
@@ -10,8 +13,25 @@ export class HomeComponent implements OnInit {
     { title: 'Home', url: 'scanner', icon: 'mail' },
     { title: 'Settings', url: 'settings', icon: 'paper-plane' }
   ];
-  constructor() { }
+  isloading: boolean;
+  constructor(
+    private authenticationService: AuthenticationService,
+    private userService: UserService) { }
 
-  ngOnInit() {}
+  get user(): User{
+    return this.userService.user;
+  }
+  ngOnInit() {
+    this.isloading = true;
+    this.userService.getUser().then((response: User) => {
+      this.isloading = false;
+    }, (err) => {
+      this.isloading = false;
+    });
+  }
+
+  logout(){
+    this.authenticationService.logout();
+  }
 
 }
---
+++ safeRunner/src/app/services/user/user.service.ts
@@ -0,0 +1,27 @@
+import { Router } from '@angular/router';
+import { HttpService } from './../http/http.service';
+import { Injectable } from '@angular/core';
+import { User } from 'src/assets/models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  user: User;
+  private baseUrl = '/user';
+
+  constructor(private httpService: HttpService, private router: Router) { }
+
+  public getUser(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.httpService.get(this.baseUrl).subscribe((response: any) => {
+        this.user = response.result;
+        resolve(response.result);
+      }, (err) => {
+        reject(err.message);
+        localStorage.clear();
+        this.router.navigate(['login']);
+      });
+    });
+  }
+}
